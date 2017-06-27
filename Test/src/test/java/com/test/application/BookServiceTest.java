83a4926a3f95bbf0ef885a24f438dc84041ab2d7
krystedt <karl.rystedt@gmail.com>
2017-06-27 14:58:57 +0200

    asdf

---
+++ Test/src/test/java/com/test/application/BookServiceTest.java
@@ -0,0 +1,16 @@
+package com.test.application;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BookServiceTest {
+
+	@Test
+	public void aTest() throws Exception {
+		Assert.assertTrue(true);
+	}
+	
+}
